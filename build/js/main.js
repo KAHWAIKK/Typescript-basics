df75114f7703d4b8cd227e4b6cc0a251632e8470
KAHWAIKK <kahwaikagwima@gmail.com>
2024-02-13 18:51:59 +0300

    Typescript terminology & configSettings

---
+++ build/js/main.js
@@ -0,0 +1,34 @@
+"use strict";
+let myName = 'Kevin'; //infer and implicitly declare the dat type
+let myNameA = 'Kevin'; //infer and explicitly declare the data type
+//Alternatively we can
+let myNameB;
+myNameB = 'Kevin';
+myNameB = 'Kagwima';
+//Extra examples
+let myNameC;
+let meaningOfLife;
+let isLoading; //union data type
+let album; //iN ts ANY IS ALSO A DATA TYPE, you can put a string,number or boolean as the value of the album without encountering any errors
+myNameC = 'Kahwa';
+meaningOfLife = 42;
+isLoading = false;
+isLoading = 5;
+album = null;
+album = 42;
+album = '4 Your Eyes Only';
+album = false;
+//Function Parameters
+const sum = (a = 2, b = '2', c = 2) => {
+    return a + b - c;
+    //you need to explixitly infer the data type in the patameters
+};
+let a = 2;
+let b = '2';
+let c = 2;
+console.log(sum);
+//union types
+let postId;
+let isActive;
+//A data typee that is mostly used to convert an existing project to a TS project
+let re = /\w+/g; //this is a regular ecxpression that would basically select all words
